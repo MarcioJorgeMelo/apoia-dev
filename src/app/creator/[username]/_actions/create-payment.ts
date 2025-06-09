d7372a76c5ac8b78d7207685bfd49e4f56d9e46d
MarcioJorgeMelo <marciojorgemedeirosmelofilho@gmail.com>
2025-06-09 18:24:22 -0300

    refactor: reajusting return datas

--- src/app/creator/[username]/_actions/create-payment.ts
+++ src/app/creator/[username]/_actions/create-payment.ts
@@ -19,14 +19,12 @@ export async function createPayment(data: CreatePaymentSchema) {
 
   if (!schema.success) {
     return {
-      data: null,
       error: schema.error.issues[0].message,
     };
   }
 
   if (!data.creatorId) {
     return {
-      data: null,
       error: "Criador não encontrado",
     };
   }
@@ -40,7 +38,6 @@ export async function createPayment(data: CreatePaymentSchema) {
 
     if (!creator) {
       return {
-        data: null,
         error: "Falha ao criar pagamento",
       };
     }
@@ -88,12 +85,10 @@ export async function createPayment(data: CreatePaymentSchema) {
     });
 
     return {
-      data: JSON.stringify(session),
-      error: null,
+      sessionId: session.id,
     };
   } catch (error) {
     return {
-      data: null,
       error: "Falha ao criar o pagamento",
     };
   }
